a905c34c20c505beaa90441730f046135e64f2ff
ZihaoWangZihao <63277556+ZihaoWangZihao@users.noreply.github.com>
2023-04-13 08:34:36 -0400

    Add files via upload

---
+++ ADHD_Jobs_Research_Repository/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using ML to find patternss between people with ADHD and their jobs.',
+    author='Zihao Wang',
+    license='MIT',
+)
